f1485ba29594e50649db7b2cfb0640d0130350fd
gmossman1 <gmossman1@studentmail.nait.ca>
2015-11-19 14:51:01 -0700

    Setup
    Startup of the project

---
+++ Tools Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tools_Website.Startup))]
+namespace Tools_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
